eceeae4b1a5ee6c210f3911ab54002d53268701d
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-11-24 23:10:53 +0530

    node api signup signin setup

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+// bring routes
+const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
+
+// app
+const app = express();
+
+mongoose
+    .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// cors
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+app.use(cors());
+
+
+// routes middleware
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
