8be9699df38444f6249232a27b1337087940d050
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-11-30 18:40:22 +0530

    contact page for sending mail added

--- backend/controllers/form.js
+++ backend/controllers/form.js
@@ -7,13 +7,13 @@ exports.contactForm = (req, res) => {
 
     const emailData = {
         to: process.env.EMAIL_TO,
-        from: email,
+        from: "vasurajanseptember16@gmail.com",
         subject: `Contact form - ${process.env.APP_NAME}`,
-        text: `Email received from contact from \n Sender name: ${name} \n Sender email: ${email} \n Sender message: ${message}`,
+        text: `Email received from contact from \n Sender name: ${name} \n Sender email: "vasurajanseptember16@gmail.com" \n Sender message: ${message}`,
         html: `
             <h4>Email received from contact form:</h4>
             <p>Sender name: ${name}</p>
-            <p>Sender email: ${email}</p>
+            <p>Sender email: "vasurajanseptember16@gmail.com"</p>
             <p>Sender message: ${message}</p>
             <hr />
             <p>This email may contain sensetive information</p>
